c04a864e74efb12c88b5ac84ef41505c82377037
David Marchante <davidmarchan@gmail.com>
2018-06-30 21:05:09 +0200

    Add GoogleDriveService

--- lib/dev_training_bot.rb
+++ lib/dev_training_bot.rb
@@ -1,5 +1,5 @@
-require "dev_training_bot/version"
+require 'dev_training_bot/version'
+require 'dev_training_bot/services/google_drive_service'
 
 module DevTrainingBot
-  # Your code goes here...
 end
