3db2de98c0fda9af8e8196272b50e4bf461ae909
David Marchante <davidmarchan@gmail.com>
2018-07-01 16:12:01 +0200

    Rename files

--- lib/dev_training_bot.rb
+++ lib/dev_training_bot.rb
@@ -3,7 +3,7 @@ require 'dotenv/load'
 require 'dev_training_bot/cli'
 require 'dev_training_bot/services/google_drive_service'
 require 'dev_training_bot/services/slack_service'
-require 'dev_training_bot/services/topics_service'
+require 'dev_training_bot/services/topic_service'
 require 'dev_training_bot/version'
 
 module DevTrainingBot
